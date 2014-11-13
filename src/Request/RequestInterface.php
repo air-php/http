d4a36be4082fbfd353d29b4ba973c89b8d13cea5
Mark Locker <mark.locker@easyfundraising.org.uk>
2014-11-13 13:28:09 +0000

    Added request method constants.

--- src/Request/RequestInterface.php
+++ src/Request/RequestInterface.php
@@ -4,6 +4,17 @@ namespace Air\HTTP\Request;
 
 interface RequestInterface
 {
+    const METHOD_CONNECT = 'CONNECT';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_GET = 'GET';
+    const METHOD_HEAD = 'HEAD';
+    const METHOD_OPTIONS = 'OPTIONS';
+    const METHOD_PATCH = 'PATCH';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_TRACE = 'TRACE';
+
+
     /**
      * @return string The request method.
      */
